a6932dce5e4f1b8a5b47ef48d74d0588e1c341a1
educia27 <edd1114@gmail.com>
2021-12-15 07:28:31 -0600

    use custom data pipe for transforming satellite count

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { OrbitListComponent } from './orbit-list/orbit-list.component';
 import { OrbitCountsComponent } from './orbit-counts/orbit-counts.component';
+import { UniquePipe } from './orbit-counts/filteredSatelliteCounts.pipe'
 
 @NgModule({
   declarations: [
     AppComponent,
     OrbitListComponent,
-    OrbitCountsComponent
+    OrbitCountsComponent,
+    UniquePipe
   ],
   imports: [
     BrowserModule
